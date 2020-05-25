833806cad692c03caecf7c2bbc90df534a2a2a36
Luke Shay <shay.luke17@gmail.com>
2020-05-25 16:42:28 -0500

    Fixed a lot of stuff.

--- tools/api/.eslintrc.js
+++ tools/api/.eslintrc.js
@@ -2,6 +2,7 @@ require('@lukeshay/eslint-config-typescript/patch');
 
 module.exports = {
   extends: ['@lukeshay/eslint-config-typescript'],
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
   env: {
     browser: true,
     jest: true,
