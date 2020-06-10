437b5447df3a63c135d864ba308813dad26abcf0
Luke Shay <lshay@sourceallies.com>
2020-06-10 15:20:12 -0500

    Some more files.

---
+++ apps/web/src/components/rr-navigation/test/rr-navigation.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('rr-navigation', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<rr-navigation></rr-navigation>');
+
+    const element = await page.find('rr-navigation');
+    expect(element).toHaveClass('hydrated');
+  });
+});
---
+++ apps/web/src/components/rr-navigation/test/rr-navigation.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { RrNavigation } from './rr-navigation';
+
+describe('rr-navigation', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [RrNavigation],
+      html: `<rr-navigation></rr-navigation>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <rr-navigation>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </rr-navigation>
+    `);
+  });
+});
