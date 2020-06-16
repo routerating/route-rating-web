ed7cd932105c2b3220418ffe5e3cee454da9ae2c
Luke Shay <lshay@sourceallies.com>
2020-06-15 23:42:50 -0500

    fix: fix rollup configs

--- packages/requests/rollup.config.js
+++ packages/requests/rollup.config.js
@@ -1,35 +1,4 @@
-import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import strip from '@rollup/plugin-strip';
-import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json'
+import { typescriptConfig } from '@lukeshay/rollup-config'
 
-export default {
-  input: 'src/main.ts',
-  output: [
-    {
-      file: pkg.cjs,
-      format: 'cjs',
-    },
-    {
-      file: pkg.es,
-      format: 'es',
-    },
-    {
-      file: pkg.umd,
-      format: 'umd',
-      name: 'routeRating',
-    },
-  ],
-  external: [...Object.keys(pkg.dependencies || {})],
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-    terser(),
-    resolve(),
-    commonjs(),
-    strip(),
-  ],
-};
+export default typescriptConfig(pkg)
