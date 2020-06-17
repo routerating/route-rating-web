1c9bbb48cb73fb3d3f18db334c3d4cb4d689b8f9
Luke Shay <shay.luke17@gmail.com>
2020-06-16 22:38:47 -0500

    chore: removed release scripts from package.json

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -13,10 +13,9 @@ PR_DESC="Published release on ${DATE}"
 git checkout -b ${BRANCH_NAME}
 git push --set-upstream origin ${BRANCH_NAME}
 
-yarn release
-# git add .
-# git commit -m ${COMMIT}
-# git push --set-upstream origin ${BRANCH_NAME}
+yarn
+yarn build
+HUSKY_BYPASS=true lerna publish
 
 gh pr create -t ${COMMIT} -b ${PR_DESC} -w
 
