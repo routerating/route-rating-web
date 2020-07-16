42e22213cb1aa0d13b7a654636e5a81dc9bfa753
Luke Shay <lshay@sourceallies.com>
2020-07-16 14:00:37 -0500

    ci: removed old workflows

--- .github/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./.github/generate.sh
-
-YAML=$(tail -n +28 .github/workflows/ci.yml)
-
-echo "name: CI" > .github/workflows/ci.yml
-echo "on: [push]" >> .github/workflows/ci.yml
-echo "${YAML}" >> .github/workflows/ci.yml
